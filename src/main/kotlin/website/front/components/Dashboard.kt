1df2dfb1d3dd7fe8b1cda87074664f0e5d739082
MichalRymarski <116505512+MichalRymarski@users.noreply.github.com>
2024-09-13 20:09:04 +0200

    UI: Create a static sidebar

--- src/main/kotlin/website/front/components/Dashboard.kt
+++ src/main/kotlin/website/front/components/Dashboard.kt
@@ -9,10 +9,23 @@ fun FlowContent.dashboard() = div(classes = classes("relative")) {
     HeaderNotLoggedIn()
     MenuMovingSidebar()
     MenuStaticSidebar()
-    main(classes("mx-auto","my-background", "sm:py-6", "lg:py-8", "min-h-screen", "w-full", "relative", "flex justify-center", "text-center")) {
-        attributes ["x-bind:class"] = "{ 'ml-56': sidebarOpen, 'ml-0': !sidebarOpen }"
+    main(
+        classes(
+            "mx-auto",
+            "my-background",
+            "sm:py-6",
+            "lg:py-8",
+            "min-h-screen",
+            "w-full",
+            "relative",
+            "flex justify-center",
+            "text-center",
+            "inline-block",
+        )
+    ) {
+        attributes["x-bind:class"] = "{ 'ml-56': sidebarOpen, 'ml-20': !sidebarOpen }"
 
-        div(classes = classes("inline-block","w-full", "px-8")) {
+        div(classes = classes("inline-block", "w-full", "px-8")) {
             img(classes = classes("w-full", "h-full", "object-cover")) {
                 src = "/static/background.jpg"
                 alt = "Background Image"
@@ -23,8 +36,21 @@ fun FlowContent.dashboard() = div(classes = classes("relative")) {
 }
 
 fun DIV.MenuStaticSidebar() {
-    aside(classes = classes("left-0 h-full my-sidebar")){
-
+    aside(classes = classes("left-0 min-h-screen fixed w-20 my-sidebar")) {
+        nav(classes = "pl-0") {
+            ul(classes = "space-y-1") {
+                listOf("Home", "Shorts", "Subscriptions","You").forEach { item ->
+                    li {
+                        a(href = "#", classes = "flex items-center h-16 hover:bg-gray-700 px-4 py-2 rounded-lg") {
+                            img(classes = "h-7 w-7 block") {
+                                src = "/static/${item}.svg"
+                                alt = "/static/loading.gif"
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
